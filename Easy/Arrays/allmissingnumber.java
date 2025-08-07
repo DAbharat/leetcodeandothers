a56f23f75b318aae4936284359e6c6fb1ff31403
DAbharat <bharat05jha@gmail.com>
2025-08-07 20:00:03 +0530

    .

--- Easy/Arrays/allmissingnumber.java
+++ Easy/Arrays/allmissingnumber.java
@@ -1,22 +1,21 @@
-public class missingnumber {
-    public int missingNumber(int[] arr) {
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] arr) {
         int i = 0;
         while(i<arr.length) {
-            int correctIndex = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[correctIndex]) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] <= arr.length && arr[i] != arr[correctIndex]) {
                 swap(arr, i, correctIndex);
             } else {
                 i++;
             }
         }
+        List<Integer> ans = new ArrayList<>();
         for (int j = 0; j < arr.length; j++) {
-            if(arr[j] != j) {
-                return j;
+            if(arr[j] != j+1) {
+                ans.add(j+1);
             }
         }
-
-
-        return arr.length;
+        return ans;
     }
 
     static void swap(int[] arr, int first, int second) {
