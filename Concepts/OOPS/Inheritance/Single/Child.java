5831f77e08a940fe9117370958ab99a5451b6eeb
Bharat <bharat05jha@gmail.com>
2025-08-20 15:31:19 +0530

    Delete Concepts/OOPS/Inheritance/Single directory

--- Concepts/OOPS/Inheritance/Single/Child.java
+++
@@ -1,7 +0,0 @@
-package Concepts.OOPS.Inheritance.Single;
-
-public class Child {
-    void india() {
-        System.out.println("India is one of them");
-    }
-}
--- Concepts/OOPS/Inheritance/Single/Parent.java
+++
@@ -1,7 +0,0 @@
-package Concepts.OOPS.Inheritance.Single;
-
-public class Parent {
-    void country() {
-        System.out.println("There are more than 195 countries in the world");
-    }
-}
